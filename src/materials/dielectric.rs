96395b55770d8fcf63a09a232f333dd7caafab50
SnowWalkerJ <jike3212001@163.com>
2024-11-01 14:18:50 +0800

    fix Dielectric; add Schlick's approximation for reflectance

--- src/materials/dielectric.rs
+++ src/materials/dielectric.rs
@@ -1,5 +1,6 @@
 use crate::material::Material;
 use crate::{Direction, HitRecord, Ray};
+use crate::misc::random;
 
 struct Dielectric {
     refraction_index: f32,
@@ -15,18 +16,25 @@ impl Material for Dielectric {
     fn scatter(&self, ray: &Ray, hit_record: &HitRecord, normal: Direction) -> Option<Ray> {
         let cos_theta = ray.direction.dot(normal);
         let sin_theta = (1.0 - cos_theta.powf(2.0)).sqrt();
-        let relative_refraction_index = if hit_record.is_front {self.refraction_index} else {1.0 / self.refraction_index};
+        let relative_refraction_index = if hit_record.is_front {1.0 / self.refraction_index} else {self.refraction_index};
         let target_sin = sin_theta * relative_refraction_index;
-        if target_sin > 1.0 {
+        if target_sin > 1.0 || reflectance(cos_theta, relative_refraction_index) < random() {
             // reflection
             let out_direction = ray.direction + normal * 2.0;
             Some(Ray::new(hit_record.point, out_direction.unitary()))
         } else {
             // refraction
-            let r_out_perp = (ray.direction + normal * cos_theta) * (1.0 / relative_refraction_index);
+            let r_out_perp = (ray.direction + normal * cos_theta) * relative_refraction_index;
             let r_out_parallel = normal * (1.0 - r_out_perp.dot(r_out_perp)).sqrt();
             let out_direction = r_out_perp + r_out_parallel;
             Some(Ray::new(hit_record.point, out_direction.unitary()))
         }
     }
+}
+
+fn reflectance(cosine: f32, refraction_index: f32) -> f32 {
+    // Use Schlick's approximation for reflectance.
+    let r0 = (1.0 - refraction_index) / (1.0 + refraction_index);
+    let r0 = r0 * r0;
+    r0 + (1.0 - r0) * (1.0 - cosine).powf(5.0)
 }
--- src/misc.rs
+++ src/misc.rs
@@ -1,9 +1,14 @@
 use std::fs;
 use std::io::Write;
+use rand;
 use crate::Canvas;
 
 pub fn write_ppm_to_file(canvas: &Canvas, filename: &str) {
     let ppm = canvas.to_ppm();
     let mut file = fs::File::create(filename).expect("Can't create file");
     file.write_all(ppm.as_bytes()).expect("error writing");
+}
+
+pub fn random() -> f32 {
+    rand::random::<f32>()
 }
